46b84daf31d81d478f7e89bea37653914bad1c12
jackypy <jacqueline.parra7@gmail.com>
2018-08-15 18:43:09 -0300

    ejercicio_0031_ok

--- 2 ciclos/ejercicio3.rb
+++ 2 ciclos/ejercicio3.rb
@@ -1,2 +1,17 @@
 # Mostrar todos los divisores del número 990 con:
 # while, for, times.
+
+990.times  do |i| i += 1
+  puts i if (990 % i).zero?
+end
+puts "----------------------------------------"
+for i in 1..990 do
+  puts i if (990 % i).zero?
+end
+puts "-----------------------------------------"
+
+i = 0
+while i < 990
+  i = i + 1
+  puts i if (990 % i).zero?
+end
