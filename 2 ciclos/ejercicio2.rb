ea3ca50b6fbc857d10b333813354f3c56da8b460
jackypy <jacqueline.parra7@gmail.com>
2018-08-15 18:09:07 -0300

    ejercicio_001_ok

--- 2 ciclos/ejercicio2.rb
+++ 2 ciclos/ejercicio2.rb
@@ -1,7 +1,7 @@
-# En el siguiente código reemplaza la instrucción 'while' por 'times'.
+# En el siguiente código reemplaza la instrucción 'while' por 'times'.,
 
-i = 0
-while i < 10
-  puts "Iteración #{i}"
-  i = i + 1
+10.times  do |i|
+  puts  "Iteración #{i}"
 end
+
+10.times  do |i| puts "Iteración #{i}" end
